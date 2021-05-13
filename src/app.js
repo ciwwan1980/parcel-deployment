673d71db582058e42526bc67ed0f43b27ac96fd3
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-14 01:10:49 +0200

    running-with-yarn parcel ./src/index.html

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const App = <div>Hello, world</div>;
+const App = ()=><div>Hello, world</div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
